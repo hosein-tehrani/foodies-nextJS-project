0ee12a3e5d2ad923c205e327a0cbade992946378
Hosein-tehrani <ho3einvictorho3ein@gmail.com>
2026-05-13 01:34:47 +0330

    meals page added

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,20 +1,27 @@
+import MealsGrid from "@/components/meals/MealsGrid";
+import styles from "./page.module.css";
 import Link from "next/link";
 export const metadata = {
   title: "meals page",
 };
 export default function Meals() {
   return (
-    <div>
-      <h1>Meals</h1>
-      <div>
-        <Link href="/meals/pizza">pizza</Link>
-      </div>
-      <div>
-        <Link href="/meals/sushi">sushi</Link>
-      </div>
-      <div>
-        <Link href="/meals/hamburger">hamburger</Link>
-      </div>
-    </div>
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. it is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href={"/meals/share"}>Share your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,8 +1,10 @@
+
 import Link from "next/link";
 import Image from "next/image";
 
 import styles from "./Header.module.css";
 import logo from "@/assets/logo.png";
+import NavLink from "./NavLink";
 
 export default function Header() {
   return (
@@ -14,10 +16,10 @@ export default function Header() {
       <nav className={styles.nav}>
         <ul>
           <li>
-            <Link href="/meals">Browse Meals</Link>
+            <NavLink href="/meals">Browse Meals</NavLink>
           </li>
           <li>
-            <Link href="/community">Foodies Community</Link>
+            <NavLink href="/community">Foodies Community</NavLink>
           </li>
         </ul>
       </nav>
