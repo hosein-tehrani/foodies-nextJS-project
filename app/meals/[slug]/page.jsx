43813ed8d50d3adaed742aec2d388e1df7f7e9e3
Hosein-tehrani <ho3einvictorho3ein@gmail.com>
2026-05-14 01:09:57 +0330

    meals detail page

--- app/meals/[slug]/page.jsx
+++ app/meals/[slug]/page.jsx
@@ -1,8 +1,36 @@
-export default function MealDetail({ params }) {
+import { getMeal } from "@/lib/meals";
+import Image from "next/image";
+import classes from "./page.module.css";
+import { notFound } from "next/navigation";
+export default async function MealDetail({ params }) {
+  const meal = await getMeal(params.slug);
+  if (!meal) {
+    notFound()
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
     <>
-      <h1>MealDetail</h1>
-      <p>{params.slug}</p>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt="{meal.summary}" fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
     </>
   );
 }
